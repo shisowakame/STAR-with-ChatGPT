4e9a0dc560d8ccf9aea0e9475664e8a3e1cbe1ac
shisowakame <128126711+shisowakame@users.noreply.github.com>
2024-02-29 01:17:30 +0900

    Create notion.py

---
+++ notion.py
@@ -0,0 +1,50 @@
+import os
+from pprint import pprint
+from notion_client import Client
+
+
+def fetch_unread_items_with_url(database_id, NOTION_TOKEN):
+    notion = Client(auth=NOTION_TOKEN)
+    db = notion.databases.query(database_id=database_id)
+    
+    unread_items_with_url_and_id = []
+    
+    for result in db['results']:
+        properties = result['properties']
+        page_id = result['id']
+        
+        # '記事名' と 'URL' プロパティが存在し、かつ 'read' チェックボックスが未読の場合のみ処理
+        if ('記事名' in properties and len(properties['記事名']['title']) > 0 and
+            'URL' in properties and properties['URL']['url'] is not None and
+            properties['URL']['url'] != "" and  # URLが空でないことを確認
+            'read' in properties and not properties['read']['checkbox']):
+            
+            name = properties['記事名']['title'][0]['text']['content']
+            url = properties['URL']['url']
+            
+            # URLが存在する場合のみ項目をリストに追加
+            unread_items_with_url_and_id.append({
+                'id': page_id,
+                'title': name,
+                'url': url
+            })
+            
+            # 最大5つの未読項目をリストに追加
+            if len(unread_items_with_url_and_id) == 1:
+                break
+    
+    return unread_items_with_url_and_id
+
+def mark_items_as_read(database_id, NOTION_TOKEN, items):
+    notion = Client(auth=NOTION_TOKEN)
+    
+    for item in items:
+        page_id = item['id']
+        notion.pages.update(
+            page_id=page_id,
+            properties={
+                'read': {
+                    'checkbox': True
+                }
+            }
+        )
